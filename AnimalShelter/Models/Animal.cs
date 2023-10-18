c4bd83e82251b7838fd760f2884a1ca8eee3499c
Brian Scherner <brianscherner@gmail.com>
2023-10-18 11:47:16 -0700

    Initial commit.

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,15 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+
+    public string Type { get; set; }
+
+    public string Name { get; set; }
+
+    public string AdmitDate { get; set; }
+
+    public string Breed { get; set; }
+  }
+}
